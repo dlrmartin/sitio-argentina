e02eed9c18e104ccdb559856ef9c83bf47401e79
Dwayne Macgowan <dwaynemac@gmail.com>
2012-09-11 22:57:04 -0300

    slide cycles image AND text now.

--- index.php
+++ index.php
@@ -24,18 +24,30 @@
 			<div id="sombra-cuerpo-inf"></div>
 			<div id="contenido-seccion">
 				<div id="marco-slideshow" class="izquierda">
-					<ul id="slideshow">
-						<li><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
-						<li><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
-						<li><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
+					<ul class="slideshow">
+						<li id="first-image"><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
+						<li id="second-image" class="hidden-slide"><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
+						<li id="third-image" class="hidden-slide"><img src="_imagenes/home/foto1.jpg" width="672" height="240" alt="" /></li>
 					</ul>
 					<div class="nav-slideshow"><a id="prev" href="#" title="<br />
 	Imagen Anterior"><img src="_imagenes/btn_prev.jpg" width="26" height="26" alt="Flecha izquierda" /></a> <a id="next" href="#" title="Imagen Siguiente"><img src="_imagenes/btn_next.jpg" width="26" height="26" alt="Flecha derecha" /></a></div>
 				</div>
 				
 				<div class="derecha" id="texto">
-					<h3 class="titulo-grande">Alta Performance</h3>
-					<p>El M&eacute;todo DeRose desenvuelve alta performance profesional y deportiva. Algunas de nuestras herramientas son la reeducaci&oacute;n respiratoria, la administraci&oacute;n del estr&eacute;s, las t&eacute;cnicas org&aacute;nicas que mejoran el tono muscular y la flexibilidad.</p>
+					<ul class="slideshow">
+						<li>
+							<h3 class="titulo-grande">Alta Performan 1</h3>
+							<p>El M&eacute;todo DeRose desenvuelve alta performance profesional y deportiva. Algunas de nuestras herramientas son la reeducaci&oacute;n respiratoria, la administraci&oacute;n del estr&eacute;s, las t&eacute;cnicas org&aacute;nicas que mejoran el tono muscular y la flexibilidad.</p>
+						</li>
+						<li>
+							<h3 class="titulo-grande">Alta Performan 2</h3>
+							<p>El M&eacute;todo DeRose desenvuelve alta performance profesional y deportiva. Algunas de nuestras herramientas son la reeducaci&oacute;n respiratoria, la administraci&oacute;n del estr&eacute;s, las t&eacute;cnicas org&aacute;nicas que mejoran el tono muscular y la flexibilidad.</p>
+						</li>
+						<li>
+							<h3 class="titulo-grande">Alta Performan 3</h3>
+							<p>El M&eacute;todo DeRose desenvuelve alta performance profesional y deportiva. Algunas de nuestras herramientas son la reeducaci&oacute;n respiratoria, la administraci&oacute;n del estr&eacute;s, las t&eacute;cnicas org&aacute;nicas que mejoran el tono muscular y la flexibilidad.</p>
+						</li>
+					</ul>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -47,17 +59,21 @@
 	<?php include '_partial/shared/javascripts.php' ?>
 	<script type="text/javascript">
 	$(document).ready(function(){
+		
 		$(function(){
-			$("#slideshow").cycle({
-				prev: '#prev',
-				next: '#next',
+			$(".slideshow").cycle({
+				prev: '#prev', // identifies trigger to previous
+				next: '#next', // identifies trigger to next
 				after: onAfter,
 			  timeout: 0
 			});
 		});
+
 		function onAfter(curr, next, opts) {
 		    var index = opts.currSlide;
+		    // hide prev button if on first
 		    $('#prev')[index == 0 ? 'hide' : 'show']();
+		    // hide next button if on last
 		    $('#next')[index == opts.slideCount - 1 ? 'hide' : 'show']();
 		}
 	});
